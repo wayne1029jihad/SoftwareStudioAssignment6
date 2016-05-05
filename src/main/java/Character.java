45aa980779c2ea75d53c1be6c895f55af9b0d626
wayne <wayne1029jihad@gmail.com>
2016-05-05 16:06:15 +0800

    upload Character test

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,7 +1,7 @@
 package main.java;
 
 import processing.core.PApplet;
-
+import java.util.ArrayList;
 /**
 * This class is used to store states of the characters in the program.
 * You will need to declare other variables depending on your implementation.
@@ -9,6 +9,9 @@ import processing.core.PApplet;
 public class Character {
 	
 	private MainApplet parent;
+	public float x, y, radius;
+	private String name;	
+	private ArrayList<Character> targets;
 
 	public Character(MainApplet parent){
 
@@ -17,7 +20,23 @@ public class Character {
 	}
 
 	public void display(){
-
+		parent.fill(0);
+		parent.fill(255);
+		parent.rect(x-15, y-15, 70, 20);
+		parent.fill(0);
+		if((x - parent.mouseX)*(x - parent.mouseX)+(y - parent.mouseY)* (y - parent.mouseY) < radius * radius)
+			parent.text(name, x, y);
+	}
+	
+	void mouseMoved() {
+		
+	}
+	public void addTarget(Character target){
+		targets.add(target);		
+	}
+	
+	public ArrayList<Character> getTargets(){
+		return this.targets;
 	}
 	
 }
