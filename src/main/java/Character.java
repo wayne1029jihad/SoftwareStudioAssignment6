91c77c09d272c7c89b77dd8f83e0a8847892cc27
Yeema <e2219755@gmail.com>
2016-05-09 20:54:31 +0800

    add number

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -40,3 +40,48 @@ public class Character {
 	}
 	
 }
+
+package main.java;
+
+import java.util.ArrayList;
+
+import processing.core.PApplet;
+
+/**
+* This class is used to store states of the characters in the program.
+* You will need to declare other variables depending on your implementation.
+*/
+public class Character {
+	
+	private MainApplet parent;
+	String name,colour;
+	private ArrayList<Character> targets;
+	private ArrayList<Character> values;
+	int number;
+
+	public Character(MainApplet parent,String name,String colour){
+
+		this.parent = parent;
+		this.name=name;
+		this.colour=colour;
+		targets = new ArrayList<Character>();
+		values = new ArrayList<Character>();
+	}
+
+	public void display(){
+
+	}
+	public void addTarget(Character target) {
+		this.targets.add(target);
+	}
+	public void addValue(Character value) {
+		this.values.add(value);
+	}
+	public ArrayList<Character> getTargets(){ return this.targets; }
+	public ArrayList<Character> getValues(){ return this.values; }
+	public void setnumber(int n)
+	{
+		number=n;
+	}
+}
+
